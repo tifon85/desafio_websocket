aca0d283061f6fc6821aa596092875c83fff6310
tifon85 <nico.ten85@gmail.com>
2023-01-15 01:12:36 -0300

    primer desafio

--- desafio.js
+++ desafio.js
@@ -1,17 +1,36 @@
 
 class ProductManager {
-    #title
-    #description
-    #price
-    #thumbnail
-    #code
-    #stock
-    constructor(title,description,price,thumbnail,code,stock){
-        this.#title=title
-        this.#description=description
-        this.#price=price
-        this.#thumbnail=thumbnail
-        this.#code=code
-        this.#stock=stock
+    constructor(){
+        this.productos=[]
     }
+
+    addProduct = (title,description,price,thumbnail,code,stock) => {
+        let yaEsta = productos.find(item=> item.code == code)
+        if(!yaEsta){
+            const producto = {
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock
+            }
+            this.productos = [...this.productos, producto]
+            if(this.productos === 0){
+                producto.id=1
+            }else{
+                producto.id=this.productos[this.productos.length-1].id + 1
+            }
+        }
+    }
+
+    getProducts = () => this.productos
+
+    getProductById = (id) => this.productos.filter(item => item.id == id)
+
 }
+
+const ProductManager = new ProductManager()
+//console.log(ProductManager.getProducts())
+//console.log(ProductManager.getProductById(id))
+//ProductManager.addProduct('asd', 'dsa', '14.23', 'ert', 'qwe', 12)
