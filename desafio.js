f052134b4d00e870ee3b90ebf9a89fa63e75ab79
tifon85 <nico.ten85@gmail.com>
2023-01-15 00:09:00 -0300

    primer desafio

---
+++ desafio.js
@@ -0,0 +1,17 @@
+
+class ProductManager {
+    #title
+    #description
+    #price
+    #thumbnail
+    #code
+    #stock
+    constructor(title,description,price,thumbnail,code,stock){
+        this.#title=title
+        this.#description=description
+        this.#price=price
+        this.#thumbnail=thumbnail
+        this.#code=code
+        this.#stock=stock
+    }
+}
