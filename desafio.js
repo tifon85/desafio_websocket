64261a0fc6b776ca0ede1858b44332191588a725
tifon85 <nico.ten85@gmail.com>
2023-01-20 00:43:57 -0300

    Segundo desafio

--- desafio.js
+++ desafio.js
@@ -1,11 +1,11 @@
 
-class ProductManager {
+class ProductManager{
     constructor(){
         this.productos=[]
     }
 
     addProduct = (title,description,price,thumbnail,code,stock) => {
-        let yaEsta = productos.find(item=> item.code == code)
+        let yaEsta = this.productos.find(item=> item.code == code)
         if(!yaEsta){
             const producto = {
                 title,
@@ -15,22 +15,37 @@ class ProductManager {
                 code,
                 stock
             }
-            this.productos = [...this.productos, producto]
-            if(this.productos === 0){
+            if(this.productos == 0){ 
                 producto.id=1
             }else{
                 producto.id=this.productos[this.productos.length-1].id + 1
             }
+            this.productos = [...this.productos, producto]
+            //console.log(this.productos)
+            return 'El producto ya fue agregado'
+        }else{
+            return 'El producto ya existe'
         }
     }
 
-    getProducts = () => this.productos
+    getProducts = () => {
+        return this.productos
+    }
 
-    getProductById = (id) => this.productos.filter(item => item.id == id)
+    getProductById = (id) => {
+        let yaEsta = this.productos.filter(item => item.id == id)
+        if(yaEsta){
+            return yaEsta
+        }else{
+            return 'No se encontró el producto'
+        }
+    }
 
 }
 
-const ProductManager = new ProductManager()
-//console.log(ProductManager.getProducts())
-//console.log(ProductManager.getProductById(id))
-//ProductManager.addProduct('asd', 'dsa', '14.23', 'ert', 'qwe', 12)
+const products = new ProductManager()
+//console.log(products.addProduct('asd', 'dsa', '14.23', 'ert', 'qwe', 12))
+//console.log(products.addProduct('asd', 'dsa', '14.23', 'ert', 'qwa', 12))
+//console.log(products.addProduct('asd', 'dsa', '14.23', 'ert', 'qwi', 12))
+//console.log(products.getProducts())
+//console.log(products.getProductById(3))
